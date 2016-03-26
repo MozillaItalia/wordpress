abdd078f7b76a568cb433be1f5caba2e408ed5de
Francesco Lodolo (:flod) <flod@lodolo.net>
2016-03-26 12:56:18 +0100

    Fix coding style in CSS, remove Download SMF from sidebar

--- themes/mozillaitalia/menu_laterale.php
+++ themes/mozillaitalia/menu_laterale.php
@@ -5,16 +5,15 @@
 	<li><a href="<?php echo $indirizzo_base_url; ?>prodotti/mozilla-thunderbird/" title="Mozilla Thunderbird">Mozilla Thunderbird</a></li>
 	<li><a href="<?php echo $indirizzo_base_url; ?>prodotti/seamonkey" title="SeaMonkey">SeaMonkey</a></li>
 	<li><a href="<?php echo $indirizzo_base_url; ?>prodotti/lightning/" title="Lightning">Lightning</a></li>
-	<li><a href="<?php echo $indirizzo_base_url; ?>prodotti/componenti-aggiuntivi/" title="Estensioni, temi e plugin">Componenti aggiuntivi</a></li>	
+	<li><a href="<?php echo $indirizzo_base_url; ?>prodotti/componenti-aggiuntivi/" title="Estensioni, temi e plugin">Componenti aggiuntivi</a></li>
   </ul>
   <h3>Download</h3>
   <ul>
 	<li><a href="<?php echo $indirizzo_base_url; ?>download#firefox" title="Mozilla Firefox">Mozilla Firefox</a></li>
-	<li><a href="<?php echo $indirizzo_base_url; ?>download#thunderbird" title="Mozilla Thunderbird">Mozilla Thunderbird</a></li>	
+	<li><a href="<?php echo $indirizzo_base_url; ?>download#thunderbird" title="Mozilla Thunderbird">Mozilla Thunderbird</a></li>
 	<li><a href="<?php echo $indirizzo_base_url; ?>download#seamonkey" title="Seamonkey">Seamonkey</a></li>
-	<li><a href="<?php echo $indirizzo_base_url; ?>download#lightning" title="Lightning">Lightning</a></li>	
-	<li><a href="<?php echo $indirizzo_base_url; ?>download#smf" title="Simple Machines Forum">SMF</a></li>	
-  </ul>  
+	<li><a href="<?php echo $indirizzo_base_url; ?>download#lightning" title="Lightning">Lightning</a></li>
+  </ul>
   <h3>Supporto</h3>
   <ul>
 	<li><a href="http://forum.mozillaitalia.org/" title="Forum">Forum</a></li>
@@ -31,16 +30,16 @@
   <h3>Collabora</h3>
   <ul>
 	<li><a href="<?php echo $indirizzo_base_url; ?>come-iniziare/" title="Come iniziare">Come iniziare</a></li>
-	<li><a href="<?php echo $indirizzo_base_url; ?>supporto-sul-forum/" title="Fornire supporto sul forum">Supporto sul forum</a></li>		
+	<li><a href="<?php echo $indirizzo_base_url; ?>supporto-sul-forum/" title="Fornire supporto sul forum">Supporto sul forum</a></li>
 	<li><a href="<?php echo $indirizzo_base_url; ?>localizzare-le-estensioni/" title="Come localizzare le estensioni">Traduzione estensioni</a></li>
 	<li><a href="<?php echo $indirizzo_base_url; ?>regole-generali-di-traduzione/" title="Stile di traduzione">Stile di traduzione</a></li>
 	<li><a href="http://forum.mozillaitalia.org/index.php#7" title="Forum traduzioni">Forum traduzioni</a></li>
-	<li><a href="<?php echo $indirizzo_base_url; ?>sumo/" title="Documentazione ufficiale">SUMO</a></li>								
+	<li><a href="<?php echo $indirizzo_base_url; ?>sumo/" title="Documentazione ufficiale">SUMO</a></li>
   </ul>
   <h3>Varie</h3>
   <ul>
 	<li><a href="<?php echo $indirizzo_base_url; ?>manifesto-mozilla/" title="">Mozilla Manifesto</a></li>
 	<li><a href="<?php echo $indirizzo_base_url; ?>motori-di-ricerca/" title="">Plugin motori di ricerca</a></li>
-	<li><a href="<?php echo $indirizzo_base_url; ?>dizionari/" title="">Dizionari</a></li>	
+	<li><a href="<?php echo $indirizzo_base_url; ?>dizionari/" title="">Dizionari</a></li>
 	<!-- ><li><a href="<?php echo $indirizzo_base_url; ?>" title="">Collaboratori</a></li>	-->
   </ul>
